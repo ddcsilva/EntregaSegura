ba8f5c2b8a38e15a292303b0b63a3fa670534e4e
Danilo <danilo.silva@msn.com>
2023-05-09 10:36:30 -0300

    Injetando o Contexto na Controller

--- Back/src/EntregaSegura.API/Controllers/CondominiosController.cs
+++ Back/src/EntregaSegura.API/Controllers/CondominiosController.cs
@@ -1,3 +1,4 @@
+using EntregaSegura.API.Contexts;
 using EntregaSegura.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,50 +8,22 @@ namespace EntregaSegura.API.Controllers;
 [ApiController]
 public class CondominiosController
 {
-    public IEnumerable<Condominio> condominios = new Condominio[] {
-        new Condominio() {
-            Nome = "Condomínio 1",
-            CNPJ = "12345678901234",
-            Telefone = "1234567890",
-            Email = "contato@condominio.com.br",
-            Logradouro = "Rua 1",
-            Numero = "123",
-            Complemento = "Apto 1",
-            CEP = "12345678",
-            Bairro = "Bairro 1",
-            Cidade = "Cidade 1",
-            Estado = "Estado 1"
-        },
-        new Condominio() {
-            Nome = "Condomínio 2",
-            CNPJ = "12345678901234",
-            Telefone = "1234567890",
-            Email = "contato@condominio2.com.br",
-            Logradouro = "Rua 2",
-            Numero = "123",
-            Complemento = "Apto 2",
-            CEP = "12345678",
-            Bairro = "Bairro 2",
-            Cidade = "Cidade 2",
-            Estado = "Estado 2"
-        }
-    };
+    private readonly EntregaSeguraContext _context;
+
+    public CondominiosController(EntregaSeguraContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
     public IEnumerable<Condominio> Get()
     {
-        return condominios;
+        return _context.Condominios;
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<Condominio> GetById(Guid id)
+    public Condominio GetById(Guid id)
     {
-        return condominios.Where(c => c.Id == id);
-    }
-
-    [HttpPost]
-    public void Post([FromBody] Condominio condominio)
-    {
-        condominios.Append(condominio);
+        return _context.Condominios.FirstOrDefault(x => x.Id == id);
     }
 }
