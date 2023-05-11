3b4ed4f75729f09d403226c29b8536c5fd99bba8
Danilo <danilo.silva@msn.com>
2023-05-11 12:48:59 -0300

    Configurando Injeção de Dependências

--- Back/src/EntregaSegura.API/Program.cs
+++ Back/src/EntregaSegura.API/Program.cs
@@ -1,4 +1,7 @@
+using EntregaSegura.Domain.Interfaces.Repositories;
 using EntregaSegura.Infrastructure.Contexts;
+using EntregaSegura.Infrastructure.Repositories;
+using EntregaSegura.Infrastructure.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +12,16 @@ builder.Services.AddDbContext<EntregaSeguraContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<EntregaSeguraContext>();
+
+builder.Services.AddScoped<ICondominioRepository, CondominioRepository>();
+builder.Services.AddScoped<IEntregaRepository, EntregaRepository>();
+builder.Services.AddScoped<IFuncionarioRepository, FuncionarioRepository>();
+builder.Services.AddScoped<IMoradorRepository, MoradorRepository>();
+builder.Services.AddScoped<ITransportadoraRepository, TransportadoraRepository>();
+builder.Services.AddScoped<IUnidadeRepository, UnidadeRepository>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
