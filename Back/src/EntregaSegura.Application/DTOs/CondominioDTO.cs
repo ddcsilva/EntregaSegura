f74fdde534549f679b764570d3d4c208a2dec958
Danilo <danilo.silva@msn.com>
2023-05-13 13:22:26 -0300

    Melhoria da validação dos DataAnnotations

--- Back/src/EntregaSegura.Application/DTOs/CondominioDTO.cs
+++ Back/src/EntregaSegura.Application/DTOs/CondominioDTO.cs
@@ -17,11 +17,6 @@ public class CondominioDTO
     public string CNPJ { get; set; }
 
     [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-    [StringLength(8, ErrorMessage = "O campo deve ter {1} caracteres.")]
-    [RegularExpression("^[0-9]*$", ErrorMessage = "O CEP deve conter apenas números.")]
-    public string CEP { get; set; }
-
-    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
     [StringLength(11, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 10)]
     [RegularExpression("^[0-9]*$", ErrorMessage = "O CEP deve conter apenas números.")]
     public string Telefone { get; set; }
@@ -38,15 +33,20 @@ public class CondominioDTO
     [StringLength(10, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.")]
     public string Numero { get; set; }
 
-    [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.")]
+    [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.")]
     public string Complemento { get; set; }
 
     [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-    [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 2)]
+    [StringLength(8, ErrorMessage = "O campo deve ter {1} caracteres.")]
+    [RegularExpression("^[0-9]*$", ErrorMessage = "O CEP deve conter apenas números.")]
+    public string CEP { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 2)]
     public string Bairro { get; set; }
 
     [Required(ErrorMessage = "O campo {0} é obrigatório.")]
-    [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 2)]
+    [StringLength(50, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres.", MinimumLength = 2)]
     public string Cidade { get; set; }
 
     [Required(ErrorMessage = "O campo {0} é obrigatório.")]
