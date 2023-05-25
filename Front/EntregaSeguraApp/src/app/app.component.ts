158dcdc39d8077fb2a6813fe01c78fe202e71069
Danilo <danilo.silva@msn.com>
2023-05-25 07:53:12 -0300

    Layout Inicial Angular Material

--- Front/EntregaSeguraApp/src/app/app.component.ts
+++ Front/EntregaSeguraApp/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'EntregaSeguraApp';
+export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
+  public mediaQueryDispositivoMovel: MediaQueryList;
+  public fotoPerfilUsuario: string = '';
+  public iniciaisUsuario: string = '';
+
+  @ViewChild('sidenav', { static: false }) sidenav!: MatSidenav;
+
+  constructor(private router: Router, public route: ActivatedRoute, private changeDetectorRef: ChangeDetectorRef) {
+    this.mediaQueryDispositivoMovel = window.matchMedia('(max-width: 600px)');
+  }
+
+  get dispositivoMovel(): boolean {
+    return this.mediaQueryDispositivoMovel.matches;
+  }
+
+  set dispositivoMovel(value: boolean) {
+    this.sidenav.opened = !value;
+    this.changeDetectorRef.detectChanges();
+  }
+
+  private definirIniciaisUsuario(): void {
+    let nomes = 'Danilo Silva'.split(' ');
+    this.iniciaisUsuario = nomes[0].charAt(0) + nomes[nomes.length - 1].charAt(0);
+  }
+
+  ngOnInit(): void {
+    this.definirIniciaisUsuario();
+  }
+
+  ngAfterViewInit(): void {
+    this.mediaQueryDispositivoMovel.addEventListener('change', () => {
+      this.dispositivoMovel = this.mediaQueryDispositivoMovel.matches;
+    });
+
+    this.dispositivoMovel = this.mediaQueryDispositivoMovel.matches;
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 }
--- Front/EntregaSeguraApp/src/app/app.module.ts
+++ Front/EntregaSeguraApp/src/app/app.module.ts
@@ -4,6 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule} from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatInputModule} from '@angular/material/input';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -12,9 +31,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatSortModule,
+    CdkTableModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatExpansionModule,
+    MatMenuModule,
+    MatBadgeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [CdkTableModule, MatTableModule]
 })
 export class AppModule { }
