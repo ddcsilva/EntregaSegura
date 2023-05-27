ce51d8995adc0246bab7b9165eafc17b459db99a
Danilo <danilo.silva@msn.com>
2023-05-27 20:12:20 -0300

    Dashboard

--- Front/EntregaSeguraApp/src/app/components/dashboard/dashboard.component.ts
+++ Front/EntregaSeguraApp/src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  totalEntregas = 1200;
+  entregasPendentes = 30;
+  unidadesOcupadas = 150;
+
+  dadosTransportadora = [
+    ['Transportadora', 'Entregas'],
+    ['Transportadora 1', 400],
+    ['Transportadora 2', 350],
+    ['Transportadora 3', 300],
+  ];
+
+  dadosEntregasPorMes = [
+    ['Mês', 'Entregas'],
+    ['Janeiro', 100],
+    ['Fevereiro', 120],
+    ['Março', 130],
+    ['Abril', 90],
+    ['Maio', 115],
+    ['Junho', 95],
+    ['Julho', 130],
+    ['Agosto', 120],
+    ['Setembro', 110],
+    ['Outubro', 100],
+    ['Novembro', 120],
+    ['Dezembro', 140]
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
