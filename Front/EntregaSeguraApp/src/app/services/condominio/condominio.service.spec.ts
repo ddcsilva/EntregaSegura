a4af30b6ce0a060610e74d77833bf43f8f2da68c
Danilo <danilo.silva@msn.com>
2023-06-01 23:57:11 -0300

    Iniciando Testes Unitários de CondominioService

--- Front/EntregaSeguraApp/src/app/services/condominio/condominio.service.spec.ts
+++ Front/EntregaSeguraApp/src/app/services/condominio/condominio.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CondominioService } from './condominio.service';
+import { TratamentoErrosService } from 'src/app/shared/services/tratamento-erros/tratamento-erros.service';
 
 describe('CondominioService', () => {
   let service: CondominioService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CondominioService,
+        TratamentoErrosService 
+      ]
+    });
+
     service = TestBed.inject(CondominioService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it('deve ser criado', () => {
+    expect(service).toBeTruthy();
+  });  
+
 });
