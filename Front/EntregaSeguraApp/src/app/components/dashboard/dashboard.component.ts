4bed2454d3cd48397c26c1f9fa52b85dd41321de
Danilo <danilo.silva@msn.com>
2023-06-07 17:47:47 -0300

    Dashboard

--- Front/EntregaSeguraApp/src/app/components/dashboard/dashboard.component.ts
+++ Front/EntregaSeguraApp/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,29 +12,56 @@ export class DashboardComponent implements OnInit {
   totalEntregas = 1200;
   entregasPendentes = 30;
   unidadesOcupadas = 150;
+  entregasConcluidas = 1050;
 
-  dadosTransportadora = [
-    ['Transportadora', 'Entregas'],
-    ['Transportadora 1', 400],
-    ['Transportadora 2', 350],
-    ['Transportadora 3', 300],
-  ];
+  public lineChartData: ChartConfiguration['data'] = {
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Entregas pendentes',
+        backgroundColor: 'rgba(148,159,177,0.2)',
+        borderColor: '#673AB7',
+        pointBackgroundColor: '#673AB7',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: '#673AB7',
+        fill: 'origin',
+      },
+      {
+        data: [ 28, 48, 40, 19, 86, 27, 90 ],
+        label: 'Entregas concluídas',
+        backgroundColor: 'rgba(77,83,96,0.2)',
+        borderColor: '#E91E63',
+        pointBackgroundColor: '#E91E63',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: '#E91E63',
+        fill: 'origin',
+      }
+    ],
+    labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho']
+  };
 
-  dadosEntregasPorMes = [
-    ['Mês', 'Entregas'],
-    ['Janeiro', 100],
-    ['Fevereiro', 120],
-    ['Março', 130],
-    ['Abril', 90],
-    ['Maio', 115],
-    ['Junho', 95],
-    ['Julho', 130],
-    ['Agosto', 120],
-    ['Setembro', 110],
-    ['Outubro', 100],
-    ['Novembro', 120],
-    ['Dezembro', 140]
-  ];
+  public lineChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    elements: {
+      line: {
+        tension: 0.5
+      }
+    },
+    scales: {
+      y: {
+        position: 'left',
+      }
+    },
+    plugins: {
+      legend: { display: true }
+    }
+  };
+
+  public lineChartType: ChartType = 'line';
+
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
   constructor() { }
 
