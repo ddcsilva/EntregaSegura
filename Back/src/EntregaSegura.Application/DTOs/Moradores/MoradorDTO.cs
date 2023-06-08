1e175ae62ebfe8ad4a9b07418ec8f0b4f4e4a1a2
Danilo <danilo.silva@msn.com>
2023-06-08 09:40:10 -0300

    Criação do DTO de Morador

---
+++ Back/src/EntregaSegura.Application/DTOs/Moradores/MoradorDTO.cs
@@ -0,0 +1,18 @@
+namespace EntregaSegura.Application.DTOs;
+
+public class MoradorDTO
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public string Email { get; set; }
+
+    public string Telefone { get; set; }
+
+    public string Ramal { get; set; }
+
+    public string NomeCondominio { get; set; }
+
+    public string BlocoAndarUnidade { get; set; }
+}
