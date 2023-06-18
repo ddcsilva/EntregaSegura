b807729fb366502933dd618d9e3a2e099e6f4e28
Danilo <danilo.silva@msn.com>
2023-06-18 16:53:08 -0300

    Teste com Token

--- Front/EntregaSeguraApp/src/app/services/condominio/condominio.service.ts
+++ Front/EntregaSeguraApp/src/app/services/condominio/condominio.service.ts
@@ -16,6 +16,7 @@ import { TratamentoErrosService } from 'src/app/shared/services/tratamento-erros
 @Injectable()
 export class CondominioService {
   private urlBase: string = 'https://localhost:5001/api/condominios';
+  private tokenHeader = new HttpHeaders({ 'Authorization': `Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwibmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBsb2NhbGhvc3QiLCJyb2xlIjoiQWRtaW4iLCJuYmYiOjE2ODcxMTc3MDgsImV4cCI6MTY4NzE0NjUwOCwiaWF0IjoxNjg3MTE3NzA4fQ.bvJ21mHP1XG1KsH71cSG8aqqVMK-HGxQiPYoLtvWQsca6GDxQWBIHhoBgdyU5wO-AD5Fe210RAMv81S2av3LKw` }); 
 
   private httpOptions: { headers: HttpHeaders } = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -24,7 +25,7 @@ export class CondominioService {
   constructor(private http: HttpClient, private tratamentoErrosService: TratamentoErrosService) { }
 
   public obterTodos(): Observable<Condominio[]> {
-    return this.fazerRequisicao(() => this.http.get<ApiResponse<Condominio[]>>(this.urlBase))
+    return this.fazerRequisicao(() => this.http.get<ApiResponse<Condominio[]>>(this.urlBase, { headers: this.tokenHeader }))
       .pipe(
         map(response => response.data)
       );
