5105dbc511d123af9d1a5c7b5e079200ca13259c
Danilo Silva <danilo.silva@msn.com>
2025-05-30 12:21:07 -0300

    feat: Novo Projeto para Reescrita

---
+++ Front/entrega-segura-ng18/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'es-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'entrega-segura-ng18';
+}
